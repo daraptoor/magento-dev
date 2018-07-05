acf98997e12df0827b6b85130b2570fda13556ed
amit k <daraptoor@gmail.com>
2018-07-05 04:52:18 +0000

    first commit

---
+++ app/design/frontend/Teafloor/ThemeV1/web/js/custom.js
@@ -0,0 +1,39 @@
+require(['jquery', 'jquery/ui'], function($j){ 
+    var selectedClass = "";
+    $j(".fil-cat").click(function () {
+        selectedClass = $j(this).attr("data-rel");
+        $j("#portfolio").fadeTo(100, 0.1);
+        $j("#portfolio div").not("." + selectedClass).fadeOut().removeClass('scale-anm');
+        setTimeout(function () {
+            $j("." + selectedClass).fadeIn().addClass('scale-anm');
+            $j("#portfolio").fadeTo(300, 1);
+        }, 300);
+
+    }); 
+});
+
+
+
+function toggleSearch(id){
+    var e = document.getElementById(id);
+    if (e.style.display == 'block' || e.style.display=='')
+    {
+        e.style.display = 'none';
+    }
+    else 
+    {
+        e.style.display = 'block';
+    }
+}
+
+function incDec(id,action){
+    var textBox = document.getElementById(id);
+    if(action == '-'){
+        if(textBox != '0'){
+            textBox.value--;
+        }
+    }else{
+        textBox.value++;
+    }
+    
+}
